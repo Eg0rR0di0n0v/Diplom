074da6f47abda2763a4445bbbb29127e13e5ffb0
Eg0rR0di0n0v <Eg0rR0di0n0v@yandex.ru>
2023-03-22 06:53:28 +0300

    skeleton of the pageantry class is ready

--- src/main/java/PageEntry.java
+++ src/main/java/PageEntry.java
@@ -3,5 +3,27 @@ public class PageEntry implements Comparable<PageEntry> {
     private final int page;
     private final int count;
 
-    // ???
+    PageEntry(String pdfName, int page, int count) {
+        this.pdfName = pdfName;
+        this.page = page;
+        this.count = count;
+    }
+
+
+    @Override
+    /*
+//    PageEntry { pdf = Этапы оценки проекта_ понятия, методы и полезные инструменты.pdf, page=12, count=6 },
+    */
+    public String toString() {
+        return "PageEntry { pdf = " + pdfName
+                + ", page = " + page
+                + ", count = " + count
+                + " }, \n";
+    }
+
+    @Override
+    /*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+    public int compareTo(PageEntry o) {
+        return 0;
+    }
 }
