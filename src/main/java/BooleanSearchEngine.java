f51b64e487c79483a71f4dcdf792a9b645abcad7
Eg0rR0di0n0v <Eg0rR0di0n0v@yandex.ru>
2023-03-22 06:26:21 +0300

    first commit

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -3,11 +3,23 @@ import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+    private Map<String, List<PageEntry>> database = new HashMap<>();
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
         // прочтите тут все pdf и сохраните нужные данные,
         // тк во время поиска сервер не должен уже читать файлы
+
+        /*
+//        Map<String, Integer> freqs = new HashMap<>(); // мапа, где ключом будет слово, а значением - частота
+//        for (var word : words) { // перебираем слова
+//            if (word.isEmpty()) {
+//                continue;
+//            }
+//            word = word.toLowerCase();
+//            freqs.put(word, freqs.getOrDefault(word, 0) + 1);
+
+          */
+
     }
 
     @Override
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,5 +9,25 @@ public class Main {
         // здесь создайте сервер, который отвечал бы на нужные запросы
         // слушать он должен порт 8989
         // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
+
+        /*
+//
+//        try (ServerSocket serverSocket = new ServerSocket(8989);) { // стартуем сервер один(!) раз
+//            while (true) { // в цикле(!) принимаем подключения
+//                try (
+//                        Socket socket = serverSocket.accept();
+//                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//                        PrintWriter out = new PrintWriter(socket.getOutputStream());
+//                ) {
+//                    // обработка одного подключения
+//                }
+//            }
+//        } catch (IOException e) {
+//            System.out.println("Не могу стартовать сервер");
+//            e.printStackTrace();
+//        }
+//
+        */
+
     }
 }
